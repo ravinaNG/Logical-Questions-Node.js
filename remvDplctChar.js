ab97f9b5346b51998b5906158ce02a1b693ac20d
Ravina Kumari <ravina18@navgurukul.org>
2021-02-01 23:43:04 +0530

    remove duplicate characters from str

--- remvDplctChar.js
+++ remvDplctChar.js
@@ -1,22 +1,46 @@
 const readline = require('readline-sync');
-const userStr = readline.question("Enter a Stering you want to remove duplicate:- ")
+let userStr = readline.question("Enter a Stering you want to remove duplicate:- ")
 
-let index = 0;
-let updated_str = "";
-let checker = 0;
-while(index<userStr.length){
-    let index2 = index + 1;
-    while(index2 < userStr.length){
-        if(userStr[index] === userStr[index2]){
-            checker = index2;
-            break;
-        }
-        index2 += 1;
+function replceStrIntoLst(str){
+    let index = 0;
+    let strInList = []
+    while(index < str.length){
+        strInList.push(str[index]);
+        index += 1;
     }
-    if(index2 === userStr.length && (checker !== index || index === 0))[
-        updated_str += userStr[index]
-    ]
-    index += 1;
+    return strInList;
 }
 
+function removeDuplct(list){
+    let listOfChar = list;
+    let index = 0;
+    while(index<listOfChar.length){
+        let index2 = index + 1;
+        while(index2 < listOfChar.length){
+            if(listOfChar[index] === listOfChar[index2]){
+                listOfChar.splice(index2, 1);
+                listOfChar.splice(index, 1)
+                index = -1;
+                break;
+            }
+            index2 += 1;
+        }
+        index += 1;
+    }
+    return listOfChar;
+}
+
+function makeStr(list){
+    let index = 0;
+    let updated_str = "";
+    while(index < list.length){
+        updated_str += list[index];
+        index += 1;
+    }
+    return updated_str;
+}
+
+let strInList = replceStrIntoLst(userStr);
+let noDuplctList = removeDuplct(strInList);
+let updated_str = makeStr(noDuplctList);
 console.log(updated_str);
