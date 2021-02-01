cef72e34553da49898703c281e2c9202a41a76bc
Ravina Kumari <ravina18@navgurukul.org>
2021-02-01 22:10:09 +0530

    making string in ascending order

--- ascendingOrderStr.js
+++ ascendingOrderStr.js
@@ -8,17 +8,27 @@ const smll_alfa = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "
 const cap_alfa = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P",
                     "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
+function convertStrInList(str){
+    let index = 0;
+    let listOfStrChar = [];
+    while(index < str.length){
+        listOfStrChar.push(str[index]);
+        index += 1;
+    }
+    return listOfStrChar;
+}
+
 function indexOfStrChar(smallAlfa, capAlfa, userStr){
     let indexes = [];
-    var index = 0;
+    let index = 0;
     while(index < smallAlfa.length){
-        let alfa_index = index + 1;
+        let alfa_index = 0;
         while(alfa_index < smallAlfa.length){
             if(userStr[index] === smallAlfa[alfa_index]){
-                indexes.push(smallAlfa[alfa_index]);
+                indexes.push(alfa_index);
             }
             else if(userStr[index] === capAlfa[alfa_index]){
-                indexes.push(capAlfa[alfa_index]);
+                indexes.push(alfa_index);
             }
             alfa_index += 1;
         }
@@ -27,30 +37,36 @@ function indexOfStrChar(smallAlfa, capAlfa, userStr){
     return indexes;
 }
 
-function shortingOfIndexes(array){
+function shortingOfIndexes(array_indexes){
+    let arrayOfindexes = array_indexes;
     let index = 0;
-    while(index < array.length){
+    let firstIndex;
+    while(index < arrayOfindexes.length){
         let index2 = index+1;
-        while(index2 < array.length){
-            if(array[index2] < array[index]){
-                array[index] = array[index2];
-                array[index2] = array[index]
+        while(index2 < arrayOfindexes.length){
+            if(arrayOfindexes[index2] < arrayOfindexes[index]){
+                firstIndex = arrayOfindexes[index]
+                arrayOfindexes[index] = arrayOfindexes[index2];
+                arrayOfindexes[index2] = firstIndex
             }
+            index2 += 1
         }
+        index += 1
     }
-    return array;
+    return arrayOfindexes;
 }
 
-let array_indexes = indexOfStrChar(smll_alfa);
-let shortOfIndexes = shortOfIndexes(array_indexes);
+let array_indexes = indexOfStrChar(smll_alfa, cap_alfa, userStr);
+let shortOfIndexes = shortingOfIndexes(array_indexes);
+let userStrInList = convertStrInList(userStr);
 
 let element = 0;
 while(element < userStr.length){
     var alfa_index = shortOfIndexes[element];
-    if(smll_alfa[alfa_index] in userStr){
+    if(userStrInList.includes(smll_alfa[alfa_index])){
         ascendingOrderStr += smll_alfa[alfa_index];
     }
-    else if(cap_alfa[alfa_index] in userStr){
+    else if(userStrInList.includes(cap_alfa[alfa_index])){
         ascendingOrderStr += cap_alfa[alfa_index];
     }
     element += 1;
