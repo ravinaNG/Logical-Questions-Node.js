70d2964915347f028eaec3ae69ceb3bb5d9f66be
Ravina Kumari <ravina18@navgurukul.org>
2021-02-17 20:58:49 +0530

    I am trying to define function first and then pass the function name

--- callbackFunction.js
+++ callbackFunction.js
@@ -1,30 +1,33 @@
 var fs = require('fs');
-console.log("Program Started");
-fs.readFile('demo.txt', function(err, data){
-    if(err) return console.log(err);
 
-    console.log(data.toString());
-});
+console.log("Program Started");
+
+function reading(err, data){
+    if(err){
+        return (console.log(err));
+    }
+    return (console.log(data));
+}
+
+fs.readFile('demo.txt', reading());
 console.log("Program Ended");
 
 
 
 
+// console.log("Program Started");
+
+// fs.readFile('demo.txt', function(err, data){
+//     if(err) return console.log(err);
+
+//     console.log(data.toString());
+// });
+
+// console.log("Program Ended");
 
 
 
-
-
-
-
-
-
-
-
-
-
-// var fs = require('fs');
-// console.log("Process started");
-// var data = fs.readFileSync('demo.txt');
-// console.log(data.toString());
-// console.log("Program ended");
+// console.log("Process started");                //|
+// var data = fs.readFileSync('demo.txt');        //|Syncronous
+// console.log(data.toString());                  //|
+// console.log("Program ended");                  //|
